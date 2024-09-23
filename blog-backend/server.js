a728701023bb2258eb9386ce7fcf4861abde9e4a
shoaibisone <shoaibalisabir2525@gmail.com>
2024-09-23 17:10:30 +0200

    Added blog-frontend and blog-backend

---
+++ blog-backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const postRoutes = require('./routes/posts');
+app.use('/api/posts', postRoutes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error(err));
+
+    
+// Basic route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Blog API!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
